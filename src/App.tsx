5bb8515be98bf84f4c05704e63bc63c40f7daa71
Dru Serkes <DruSerkes@gmail.com>
2020-11-15 14:59:38 -0500

    add location callback helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { BtnLink } from './components/BtnLink';
+import { locationFail, locationSuccess } from './helpers/geoCallbacks'
+
 
 function App() {
+  const [coords, setCoords] = useState(null)
+  const coffeeLink = `https://maps.google.com/?ll=${latitude},${longitude}&q=coffee&z=15`;
+  const wineLink = `https://maps.google.com/?ll=${latitude},${longitude}&q=wine+bar&z=15`;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition();
+  }, [])
+
   return (
     <div className="App">
       <h1>Would you like...</h1>
-      {/* TODO create button anchor link component with props for text, color and href */}
+      <BtnLink text="Coffee" color="TODO" link={coffeeLink} />
       <p>or</p>
-      {/* TODO button */}
+      <BtnLink text="Wine" color="TODO" link={wineLink} />
     </div>
   );
 }
---
+++ src/helpers/geoCallbacks.ts
@@ -0,0 +1,10 @@
+export const locationSuccess = (position: Position) => {
+    const { latitude, longitude } = position.coords;
+    return { latitude, longitude };
+};
+
+
+export const locationFail = (error: PositionError) => {
+    console.log({ error });
+    window.location.reload();
+};
